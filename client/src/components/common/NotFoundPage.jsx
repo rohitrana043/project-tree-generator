f5f5455b598e5f859610af2fa7311db23ee01111
ROHIT RANA <rohit.rana043@gmail.com>
2025-03-05 01:20:52 -0500

    path fix

--- client/src/components/common/NotFoundPage.jsx
+++ client/src/components/common/NotFoundPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaHome, FaSearch } from 'react-icons/fa';
-import SEO from '../components/common/SEO';
+import SEO from './SEO';
 import './NotFoundPage.css';
 
 const NotFoundPage = () => {
